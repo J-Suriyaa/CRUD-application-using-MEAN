09426d2c428949a540440282568dc4ff075bb911
J_suriyaa <60760195+J-Suriyaa@users.noreply.github.com>
2020-07-27 12:21:07 +0530

    Delete index.model.js

--- index.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-newSchema = new Schema({
-    name:String,
-    email:String,
-    address:String,
-    position:String,
-})
-module.exports = mongoose.model('User', newSchema)
